6a08482a3d97643a458024bb03467fb38c033f7a
charmyin <976582265@qq.com>
2014-05-30 13:48:38 +0800

    Name the dir by dir count+1 is been changed to name the dir by max file name+1

--- test.sh
+++ test.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 #Author Charmyin
 ###########Auto run video capture program############
- 
- 
-fileArray=($(ls -t))
-a=1
-for fileName in "${fileArray[@]}"
-	do
-	  newFileName=$(printf "%06d.jpg" ${a}) #04 pad to length of 4
-	  echo " ${fileName} ${newFileName}"
-	  let a=a+1
-	done
+if [ -e "/nt/usb" ]; then
+	echo "hello boy"
+fi
